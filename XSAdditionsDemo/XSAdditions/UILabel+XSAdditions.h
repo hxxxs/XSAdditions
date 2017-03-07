7b6a672bb264bbdf81fcbad8ea7dcd657a0aca02
Cedar <lychae@foxmail.com>
2017-03-07 13:35:25 +0800

    UILabel+XSAdditions

---
+++ XSAdditionsDemo/XSAdditions/UILabel+XSAdditions.h
@@ -0,0 +1,22 @@
+//
+//  UILabel+XSAdditions.h
+//  XSAdditionsDemo
+//
+//  Created by Cedar on 2017/3/7.
+//  Copyright © 2017年 Cedar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (XSAdditions)
+
+/**
+ 创建文本标签
+ 
+ @param text     文本
+ @param fontSize 字体大小
+ @param color    文字颜色
+ */
++ (instancetype)xs_labelWithText:(NSString *)text fontSize:(CGFloat)fontSize color:(UIColor *)color;
+
+@end
---
+++ XSAdditionsDemo/XSAdditions/UILabel+XSAdditions.m
@@ -0,0 +1,26 @@
+//
+//  UILabel+XSAdditions.m
+//  XSAdditionsDemo
+//
+//  Created by Cedar on 2017/3/7.
+//  Copyright © 2017年 Cedar. All rights reserved.
+//
+
+#import "UILabel+XSAdditions.h"
+
+@implementation UILabel (XSAdditions)
+
++ (instancetype)xs_labelWithText:(NSString *)text fontSize:(CGFloat)fontSize color:(UIColor *)color {
+    UILabel *label = [[self alloc] init];
+    
+    label.text = text;
+    label.font = [UIFont systemFontOfSize:fontSize];
+    label.textColor = color;
+    label.numberOfLines = 0;
+    
+    [label sizeToFit];
+    
+    return label;
+}
+
+@end
