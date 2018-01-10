b17036723e133c9f290d19be7cf21aa804f9ba92
黄雪松 <huangxuesong@glzc.com>
2018-01-10 14:44:09 +0800

    按钮方法拓展

--- XSAdditionsDemo/XSAdditionsDemo/XSAdditions/UIButton+XSAdditions.h
+++ XSAdditionsDemo/XSAdditionsDemo/XSAdditions/UIButton+XSAdditions.h
@@ -10,4 +10,158 @@
 
 @interface UIButton (XSAdditions)
 
+/**
+ 创建一个按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @return 按钮
+ */
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color;
+
+/**
+ 创建一个按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ @return 按钮
+ */
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color image:(UIImage *)image;
+
+/**
+ 创建一个按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param backgroundImage 背景图片
+ @return 按钮
+ */
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage;
+
+/**
+ 创建一个按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ @param backgroundImage 背景图片
+ @return 按钮
+ */
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage;
+
+#pragma mark - setupNormal
+
+/**
+ 设置默认状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ @param backgroundImage 背景图片
+ */
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage;
+
+/**
+ 设置默认状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ */
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image;
+
+/**
+ 设置默认状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param backgroundImage 背景图片
+ */
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage;
+
+/**
+ 设置默认状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ */
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color;
+
+#pragma mark - setupSelected
+
+/**
+ 设置选中状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ @param backgroundImage 背景图片
+ */
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage;
+
+/**
+ 设置选中状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ */
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image;
+
+/**
+ 设置选中状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param backgroundImage 背景图片
+ */
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage;
+
+/**
+ 设置选中状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ */
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color;
+
+#pragma mark - setupHighlighted
+
+/**
+ 设置高亮状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ @param backgroundImage 背景图片
+ */
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage;
+
+/**
+ 设置高亮状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param image 图片
+ */
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image;
+
+/**
+ 设置高亮状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ @param backgroundImage 背景图片
+ */
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage;
+
+/**
+ 设置高亮状态按钮
+ 
+ @param title 标题
+ @param color 标题颜色
+ */
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color;
+
 @end
--- XSAdditionsDemo/XSAdditionsDemo/XSAdditions/UIButton+XSAdditions.m
+++ XSAdditionsDemo/XSAdditionsDemo/XSAdditions/UIButton+XSAdditions.m
@@ -10,6 +10,189 @@
 
 @implementation UIButton (XSAdditions)
 
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color {
+    
+    UIButton *btn = [[self alloc] init];
+    
+    [btn xs_setupNormalButton:title color:color];
+    
+    return btn;
+}
 
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color image:(UIImage *)image {
+    
+    UIButton *btn = [[self alloc] init];
+    
+    [btn xs_setupNormalButton:title color:color image:image];
+    
+    return btn;
+}
+
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage {
+    
+    UIButton *btn = [[self alloc] init];
+    
+    [btn xs_setupNormalButton:title color:color backgroundImage: backgroundImage];
+    
+    return btn;
+}
+
++ (instancetype)xs_button:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage {
+    
+    UIButton *btn = [[self alloc] init];
+    
+    [btn xs_setupNormalButton:title color:color image:image backgroundImage: backgroundImage];
+    
+    return btn;
+}
+
+#pragma mark - setupNormal
+
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage {
+    
+    [self setTitle:title forState:UIControlStateNormal];
+    [self setTitleColor:color forState:UIControlStateNormal];
+    
+    if (image != nil) {
+        [self setImage:image forState:UIControlStateNormal];
+    }
+    
+    if (backgroundImage != nil) {
+        [self setBackgroundImage:backgroundImage forState:UIControlStateNormal];
+    }
+    
+    [self sizeToFit];
+}
+
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image {
+    
+    [self setTitle:title forState:UIControlStateNormal];
+    [self setTitleColor:color forState:UIControlStateNormal];
+    
+    if (image != nil) {
+        [self setImage:image forState:UIControlStateNormal];
+    }
+
+    [self sizeToFit];
+}
+
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage {
+    
+    [self setTitle:title forState:UIControlStateNormal];
+    [self setTitleColor:color forState:UIControlStateNormal];
+
+    if (backgroundImage != nil) {
+        [self setBackgroundImage:backgroundImage forState:UIControlStateNormal];
+    }
+    
+    [self sizeToFit];
+}
+
+- (void)xs_setupNormalButton:(NSString *)title color:(UIColor *)color {
+    
+    [self setTitle:title forState:UIControlStateNormal];
+    [self setTitleColor:color forState:UIControlStateNormal];
+
+    [self sizeToFit];
+}
+
+#pragma mark - setupSelected
+
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage {
+
+    [self setTitle:title forState:UIControlStateSelected];
+    [self setTitleColor:color forState:UIControlStateSelected];
+
+    if (image != nil) {
+        [self setImage:image forState:UIControlStateSelected];
+    }
+
+    if (backgroundImage != nil) {
+        [self setBackgroundImage:backgroundImage forState:UIControlStateSelected];
+    }
+
+    [self sizeToFit];
+}
+
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image {
+
+    [self setTitle:title forState:UIControlStateSelected];
+    [self setTitleColor:color forState:UIControlStateSelected];
+
+    if (image != nil) {
+        [self setImage:image forState:UIControlStateSelected];
+    }
+
+    [self sizeToFit];
+}
+
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage {
+
+    [self setTitle:title forState:UIControlStateSelected];
+    [self setTitleColor:color forState:UIControlStateSelected];
+
+    if (backgroundImage != nil) {
+        [self setBackgroundImage:backgroundImage forState:UIControlStateSelected];
+    }
+
+    [self sizeToFit];
+}
+
+- (void)xs_setupSelectedButton:(NSString *)title color:(UIColor *)color {
+    
+    [self setTitle:title forState:UIControlStateSelected];
+    [self setTitleColor:color forState:UIControlStateSelected];
+    
+    [self sizeToFit];
+}
+
+#pragma mark - setupHighlighted
+
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image backgroundImage:(UIImage *)backgroundImage {
+    
+    [self setTitle:title forState:UIControlStateHighlighted];
+    [self setTitleColor:color forState:UIControlStateHighlighted];
+    
+    if (image != nil) {
+        [self setImage:image forState:UIControlStateHighlighted];
+    }
+    
+    if (backgroundImage != nil) {
+        [self setBackgroundImage:backgroundImage forState:UIControlStateHighlighted];
+    }
+    
+    [self sizeToFit];
+}
+
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color image:(UIImage *)image {
+    
+    [self setTitle:title forState:UIControlStateHighlighted];
+    [self setTitleColor:color forState:UIControlStateHighlighted];
+    
+    if (image != nil) {
+        [self setImage:image forState:UIControlStateHighlighted];
+    }
+    
+    [self sizeToFit];
+}
+
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color backgroundImage:(UIImage *)backgroundImage {
+    
+    [self setTitle:title forState:UIControlStateHighlighted];
+    [self setTitleColor:color forState:UIControlStateHighlighted];
+    
+    if (backgroundImage != nil) {
+        [self setBackgroundImage:backgroundImage forState:UIControlStateHighlighted];
+    }
+    
+    [self sizeToFit];
+}
+
+- (void)xs_setupHighlightedButton:(NSString *)title color:(UIColor *)color {
+    
+    [self setTitle:title forState:UIControlStateHighlighted];
+    [self setTitleColor:color forState:UIControlStateHighlighted];
+    [self sizeToFit];
+}
 
 @end
